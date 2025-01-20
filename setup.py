0844f8d3285d50df3bd738ef0608f3ba66a66c54
John | Elite Encoder <john@eliteencoder.net>
2025-01-20 13:44:21 -0500

    fix streamdiffusion install (#1)
    * Update pyproject.toml, requirements.txt, setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1.0",
     description="StreamDiffusion project dependencies",
     install_requires=[
-        "torch==2.1.0",
-        "torchvision==0.16.0",
+        "torch>=2.3.0",
+        "torchvision>=0.16.0",
         "xformers",
-        "huggingface-hub==0.23.2",
-        "diffusers==0.30.0",
-        "protobuf==5.27.2"
+        "huggingface-hub>=0.25.0",
+        "diffusers>=0.32.2",
+        "protobuf=>5.27.2"
     ],
     python_requires=">=3.10",
 )
